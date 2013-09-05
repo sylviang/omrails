148eab12c9830035e8ad108b26008212925d605f
sylviang <n.sylvia@gmail.com>
2013-09-05 21:33:41 +0800

    Add Pagination

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'devise'
 gem 'simple_form', '3.0.0.rc'
 gem 'paperclip', github: 'thoughtbot/paperclip', branch: 'rails-4'
 gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 # Use sqlite3 as the database for Active Record
 group :production do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order("created_at desc")
+    @posts = Post.order("created_at desc").page(params[:page]).per_page(20)
     #@posts = Post.all
     #to show only current_user Posts
     #@posts = current_user.posts.all
